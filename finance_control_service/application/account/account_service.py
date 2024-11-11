43231e73083f3a4c70a5e84a4ae3982435940dd4
Matheus Felipe <matheus.felipescruz@gmail.com>
2024-11-11 17:43:30 -0300

    FEAT: account domain

---
+++ finance_control_service/application/account/account_service.py
@@ -0,0 +1,29 @@
+from finance_control_service.application.account.account_storage import AccountStorage
+from finance_control_service.application.account.account_dto import AccountDTO
+from uuid import UUID
+
+class AccountService(object):
+    storage: AccountStorage
+    
+    def __init__(self, storage: AccountStorage) -> None:
+        self.storage = storage
+        
+    def get_all(self, user_id: UUID) -> list[AccountDTO]:
+        return self.storage.get_all(user_id)
+    
+    def get_by_id(self, account_id: UUID) -> AccountDTO:
+        return self.storage.get_by_id(account_id)
+    
+    def create(self, account_dto: AccountDTO) -> AccountDTO:
+        account = account_dto.to_domain()
+        account.create()
+        return self.storage.save(account_dto.to_dto(account))
+    
+    def update(self, account_dto: AccountDTO, id: UUID) -> AccountDTO:
+        account = account_dto.to_domain()
+        account.update()
+        return self.storage.update(account_dto.to_dto(account))
+    
+    def delete(self, account_id: UUID) -> bool:
+        self.storage.delete(account_id)
+        return True
---
+++ finance_control_service/application/account/account_storage.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from finance_control_service.application.account.account_dto import AccountDTO
+
+class AccountStorage(ABC):
+    
+    @abstractmethod
+    def save(self, account_dto: AccountDTO) -> AccountDTO:
+        pass
+    
+    @abstractmethod
+    def update(self, account_dto: AccountDTO) -> AccountDTO:
+        pass
+
+    @abstractmethod
+    def delete(self, account_id: UUID) -> bool:
+        pass
+    
+    @abstractmethod
+    def get_all(self, user_id: UUID) -> list[AccountDTO]:
+        pass
+    
+    @abstractmethod
+    def get_by_id(self, Account_id: UUID) -> AccountDTO:
+        pass
