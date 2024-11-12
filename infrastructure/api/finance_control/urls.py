cbfe1b60d6dfb9bde2368abc8da3a157b4150aed
Matheus Felipe <matheus.felipescruz@gmail.com>
2024-11-12 16:15:09 -0300

    FEAT: account routes

--- infrastructure/api/finance_control/urls.py
+++ infrastructure/api/finance_control/urls.py
@@ -4,6 +4,8 @@ from finance_control.views.login_view import LoginView
 from finance_control.views.user_view import UsersView
 from finance_control.views.categories_view import CategoriesView
 from finance_control.views.cards_view import CardsView
+from finance_control.views.account_view import AccountView
+
 urlpatterns = [
     path('auth/login/', LoginView.as_view(), name='login'),
     path('users/', UsersView.as_view(), name='users-list'),
@@ -12,4 +14,6 @@ urlpatterns = [
     path('categories/<uuid:pk>/', CategoriesView.as_view(), name='category-detail'),
     path('cards/', CardsView.as_view(), name='cards-list'),
     path('cards/<uuid:pk>/', CardsView.as_view(), name='card-detail'),
+    path('accounts/', AccountView.as_view(), name='accounts-list'),
+    path('accounts/<uuid:pk>/', AccountView.as_view(), name='account-detail'),
 ]
