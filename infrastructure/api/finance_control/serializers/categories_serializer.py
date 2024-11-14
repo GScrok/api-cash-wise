5c385e0eb3cb27572362980a580eb2778b26518c
Matheus Felipe <matheus.felipescruz@gmail.com>
2024-11-13 23:49:37 -0300

    FIX: Show category id on serializer

--- infrastructure/api/finance_control/serializers/categories_serializer.py
+++ infrastructure/api/finance_control/serializers/categories_serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 class CategorySerializer(serializers.Serializer):
+    
+    id = serializers.UUIDField(required=False)
     name = serializers.CharField(required=True)
     budget_limit = serializers.FloatField(required=True)
     description = serializers.CharField(required=False, allow_null=True, allow_blank=True)
