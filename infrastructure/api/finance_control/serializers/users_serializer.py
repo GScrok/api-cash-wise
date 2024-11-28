9b4abf6979d00f7cbcadc2c35ad3596428864af9
Matheus <matheus.felipescruz@gmail.com>
2024-11-28 20:01:21 -0300

    FIX: Return create user

--- infrastructure/api/finance_control/serializers/users_serializer.py
+++ infrastructure/api/finance_control/serializers/users_serializer.py
@@ -8,7 +8,7 @@ class UserCreateSerializer(serializers.Serializer):
     last_name = serializers.CharField(required=False, allow_blank=True)
 
 class UserUpdateSerializer(serializers.Serializer):
-    username = serializers.CharField(required=True)
+    username = serializers.CharField(required=True, write_only=True)
     email = serializers.EmailField(required=True)
     first_name = serializers.CharField(required=False, allow_blank=True)
     last_name = serializers.CharField(required=False, allow_blank=True)
