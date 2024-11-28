97d3e4895d0361e4b3da7c7b970a1e80718fde03
GScrok <grmscrok@gmail.com>
2024-11-28 19:44:03 -0300

    FIX: Urls

--- infrastructure/api/finance_control/urls.py
+++ infrastructure/api/finance_control/urls.py
@@ -1,17 +1,19 @@
 from django.urls import path
 
 from finance_control.views.login_view import LoginView
-from finance_control.views.user_view import UsersView
+from finance_control.views.user_view import UsersView, UserCreateView
 from finance_control.views.categories_view import CategoriesView
 from finance_control.views.cards_view import CardsView
 from infrastructure.api.finance_control.views.accounts_view import AccountView
-from finance_control.views.subcategories_view import SubcategoryView, SubcategoryCustomView
+from finance_control.views.subcategories_view import SubcategoryView, SubcategoryByCategory
 from finance_control.views.goals_view import GoalView, GoalByCategoryView
+from finance_control.views.transactions_view import TransactionView, TransactionViewByCategory, TransactionViewBySubategory
 
 urlpatterns = [
     path('auth/login/', LoginView.as_view(), name='login'),
     path('users/', UsersView.as_view(), name='users-list'),
     path('users/<uuid:pk>/', UsersView.as_view(), name='user-detail'),
+    path('users/create/', UserCreateView.as_view(), name='users-list'),
     path('categories/', CategoriesView.as_view(), name='categories-list'),
     path('categories/<uuid:pk>/', CategoriesView.as_view(), name='category-detail'),
     path('cards/', CardsView.as_view(), name='cards-list'),
@@ -20,8 +22,12 @@ urlpatterns = [
     path('accounts/<uuid:pk>/', AccountView.as_view(), name='account-detail'),
     path('subcategories/<uuid:pk>/', SubcategoryView.as_view(), name='subcategory-detail'),
     path('subcategories/', SubcategoryView.as_view(), name='subcategories-list'),
-    path('subcategories/category/<uuid:pk>/', SubcategoryCustomView.as_view(), name='subcategories-by-category'),
+    path('subcategories/category/<uuid:pk>/', SubcategoryByCategory.as_view(), name='subcategories-by-category'),
     path('goals/', GoalView.as_view(), name='goals-list'),
     path('goals/<uuid:pk>/', GoalView.as_view(), name='goal-detail'),
     path('goals/category/<uuid:pk>/', GoalByCategoryView.as_view(), name='goals-by-category'),
+    path('transactions/', TransactionView.as_view(), name='transaction-detail'),
+    path('transactions/<uuid:pk>', TransactionView.as_view(), name='transaction-list'),
+    path('transactions/category/<uuid:pk>', TransactionViewByCategory.as_view(), name='transaction-by-category'),
+    path('transactions/subcategories/<uuid:pk>', TransactionViewBySubategory.as_view(), name='transaction-by-subcategories'),
 ]
